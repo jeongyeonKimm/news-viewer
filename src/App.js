9ed1d23f5393e55d613c7b3c0e7647e6adc1cb7c
kim_jeongyeon <wjddus9132@gmail.com>
2022-02-08 14:54:37 +0900

    Issue newsapi API key

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const onClick = async () => {
     try {
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=37a57fbe88bd41c492d9895b0382b1df',
       );
       setData(response.data);
     } catch (e) {
